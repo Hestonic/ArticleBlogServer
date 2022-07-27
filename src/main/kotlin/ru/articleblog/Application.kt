3cf56f3007996bc2f7f079510e8b3667f86e720e
danii <daniilgeroldovich@gmail.com>
2022-07-27 13:36:52 +0300

    Add saving authorized users in server memory (2)

--- src/main/kotlin/ru/articleblog/Application.kt
+++ src/main/kotlin/ru/articleblog/Application.kt
@@ -2,11 +2,13 @@ package ru.articleblog
 
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import ru.articleblog.features.login.configureLoginRouting
 import ru.articleblog.plugins.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
+        configureLoginRouting()
         configureSerialization()
     }.start(wait = true)
 }
