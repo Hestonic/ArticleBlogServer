b5b5e7e7eb45f0d9010b26a4691c5b897aaa1171
danii <daniilgeroldovich@gmail.com>
2022-07-27 16:08:18 +0300

    Fix error

--- src/main/kotlin/ru/articleblog/features/register/RegisterRemote.kt
+++ src/main/kotlin/ru/articleblog/features/register/RegisterRemote.kt
@@ -1,11 +1,15 @@
 package ru.articleblog.features.register
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class RegisterReceiveRemote(
     val login: String,
     val email: String,
     val password: String
 )
 
+@Serializable
 data class RegisterResponseRemote(
     val token: String
 )
