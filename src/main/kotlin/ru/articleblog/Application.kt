3d910b00bcbff71698a80c88bd0fbdfa17a46280
danii <daniilgeroldovich@gmail.com>
2022-07-27 13:25:21 +0300

    Add Serialization.kt

--- src/main/kotlin/ru/articleblog/Application.kt
+++ src/main/kotlin/ru/articleblog/Application.kt
@@ -7,5 +7,6 @@ import ru.articleblog.plugins.*
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
+        configureSerialization()
     }.start(wait = true)
 }
