7e6a2743a65d293f52c5c688ecd95f447d0944b4
danii <daniilgeroldovich@gmail.com>
2022-07-27 13:36:28 +0300

    Add saving authorized users in server memory

---
+++ src/main/kotlin/ru/articleblog/features/cache/InMemoryCache.kt
@@ -0,0 +1,13 @@
+package ru.articleblog.features.cache
+
+import ru.articleblog.features.register.RegisterReceiveRemote
+
+data class TokenCache(
+    val login: String,
+    val token: String
+)
+
+object InMemoryCache {
+    val userList: MutableList<RegisterReceiveRemote> = mutableListOf()
+    val token: MutableList<TokenCache> = mutableListOf()
+}
