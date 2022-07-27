164d613b24056e379dab81921c5272c24aefad35
danii <daniilgeroldovich@gmail.com>
2022-07-27 16:31:19 +0300

    Add login logic

--- src/main/kotlin/ru/articleblog/Application.kt
+++ src/main/kotlin/ru/articleblog/Application.kt
@@ -3,12 +3,14 @@ package ru.articleblog
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import ru.articleblog.features.login.configureLoginRouting
+import ru.articleblog.features.register.configureRegisterRouting
 import ru.articleblog.plugins.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
         configureLoginRouting()
+        configureRegisterRouting()
         configureSerialization()
     }.start(wait = true)
 }
