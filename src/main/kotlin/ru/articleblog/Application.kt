3e190197d79182b917d11cc2f8b309eeccb5bc27
danii <daniilgeroldovich@gmail.com>
2022-07-30 13:28:18 +0300

    Add local db connection

--- src/main/kotlin/ru/articleblog/Application.kt
+++ src/main/kotlin/ru/articleblog/Application.kt
@@ -2,12 +2,20 @@ package ru.articleblog
 
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import org.jetbrains.exposed.sql.Database
 import ru.articleblog.features.login.configureLoginRouting
 import ru.articleblog.features.register.configureRegisterRouting
 import ru.articleblog.plugins.configureRouting
 import ru.articleblog.plugins.configureSerialization
 
 fun main() {
+    Database.connect(
+        "jdbc:mysql://localhost:3306/articleblog",
+        "com.mysql.cj.jdbc.Driver",
+        "root",
+        "root"
+    )
+
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
         configureLoginRouting()
