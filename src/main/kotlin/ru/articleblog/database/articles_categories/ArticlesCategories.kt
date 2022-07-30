58f9a6558b5e4a4238cb01a61c2cc3355ac88731
danii <daniilgeroldovich@gmail.com>
2022-07-30 20:23:57 +0300

    Function to add a many-to-many relationship to a table

--- src/main/kotlin/ru/articleblog/database/articles_categories/ArticlesCategories.kt
+++ src/main/kotlin/ru/articleblog/database/articles_categories/ArticlesCategories.kt
@@ -1,10 +1,21 @@
 package ru.articleblog.database.articles_categories
 
 import org.jetbrains.exposed.sql.Table
-import ru.articleblog.database.categories.Categories
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
 
 object ArticlesCategories : Table("articles_categories") {
     private val id = ArticlesCategories.integer("id")
-    private val idArticle = ArticlesCategories.integer("id_article ")
+    private val idArticle = ArticlesCategories.integer("id_article")
     private val idCategory = ArticlesCategories.integer("id_category")
+
+    fun insert(articlesCategoriesDTO: ArticlesCategoriesDTO) {
+        transaction {
+            ArticlesCategories.insert {
+                it[id] = articlesCategoriesDTO.id
+                it[idArticle] = articlesCategoriesDTO.idArticle
+                it[idCategory] = articlesCategoriesDTO.idCategory
+            }
+        }
+    }
 }
