c32bdfc4dff6b5c3a2482904ed0bdab62415d614
danii <daniilgeroldovich@gmail.com>
2022-07-30 20:22:20 +0300

    Add functions insertArticle and fetchLastArticle

--- src/main/kotlin/ru/articleblog/database/articles/ArticleDTO.kt
+++ src/main/kotlin/ru/articleblog/database/articles/ArticleDTO.kt
@@ -1,8 +1,8 @@
 package ru.articleblog.database.articles
 
-data class ArticlesDTO(
+data class ArticleDTO(
     val id: Int,
     val tittle: String,
     val text: String,
-    val idArticleInfo: Int
+    val idArticleInfo: Int,
 )
--- src/main/kotlin/ru/articleblog/database/articles/Articles.kt
+++ src/main/kotlin/ru/articleblog/database/articles/Articles.kt
@@ -1,11 +1,40 @@
 package ru.articleblog.database.articles
 
 import org.jetbrains.exposed.sql.Table
-import ru.articleblog.database.categories.Categories
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
 
 object Articles : Table("articles") {
-    private val id = Categories.integer("id")
-    private val tittle = Categories.varchar("tittle", 50)
-    private val text = Categories.text("text")
-    private val idArticleInfo = Categories.integer("id_article_info")
+    private val id = Articles.integer("id")
+    private val tittle = Articles.varchar("tittle", 50)
+    private val text = Articles.text("text")
+    private val idArticleInfo = Articles.integer("id_article_info")
+
+    fun insertArticle(articlesDTO: ArticleDTO) {
+        transaction {
+            Articles.insert {
+                it[id] = articlesDTO.id
+                it[tittle] = articlesDTO.tittle
+                it[text] = articlesDTO.text
+                it[idArticleInfo] = articlesDTO.idArticleInfo
+            }
+        }
+    }
+
+    fun fetchLastArticle(): ArticleDTO? {
+        return try {
+            transaction {
+                val articleModel = Articles.selectAll().last()
+                ArticleDTO(
+                    id = articleModel[Articles.id],
+                    text = articleModel[text],
+                    tittle = articleModel[tittle],
+                    idArticleInfo = articleModel[idArticleInfo],
+                )
+            }
+        } catch (e: Exception) {
+            null
+        }
+    }
 }
