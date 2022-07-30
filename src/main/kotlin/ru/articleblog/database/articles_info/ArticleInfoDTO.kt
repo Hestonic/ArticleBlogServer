243dd2c6c649964cba9dc191b3d7085eee95f8d6
danii <daniilgeroldovich@gmail.com>
2022-07-30 20:24:47 +0300

    Add functions insertArticleInfo and fetchArticleInfoLastRecord

--- src/main/kotlin/ru/articleblog/database/articles_info/ArticleInfoDTO.kt
+++ src/main/kotlin/ru/articleblog/database/articles_info/ArticleInfoDTO.kt
@@ -1,6 +1,6 @@
 package ru.articleblog.database.articles_info
 
-data class ArticlesInfoDTO(
+data class ArticleInfoDTO(
     val id: Int,
     val likes: Int,
     val views: Int,
--- src/main/kotlin/ru/articleblog/database/articles_info/ArticlesInfo.kt
+++ src/main/kotlin/ru/articleblog/database/articles_info/ArticlesInfo.kt
@@ -1,9 +1,37 @@
 package ru.articleblog.database.articles_info
 
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
 
-object ArticlesInfo : Table("articles_info") {
+object ArticlesInfo : Table("article_info") {
     private val id = ArticlesInfo.integer("id")
     private val likes = ArticlesInfo.integer("likes")
     private val views = ArticlesInfo.integer("views")
+
+    fun insertArticleInfo(articleInfoDTO: ArticleInfoDTO) {
+        transaction {
+            ArticlesInfo.insert {
+                it[id] = articleInfoDTO.id
+                it[likes] = articleInfoDTO.likes
+                it[views] = articleInfoDTO.views
+            }
+        }
+    }
+
+    fun fetchArticleInfoLastRecord(): ArticleInfoDTO? {
+        return try {
+            transaction {
+                val articleInfoModel = ArticlesInfo.selectAll().last()
+                ArticleInfoDTO(
+                    id = articleInfoModel[ArticlesInfo.id],
+                    likes = articleInfoModel[likes],
+                    views = articleInfoModel[views],
+                )
+            }
+        } catch (e: Exception) {
+            null
+        }
+    }
 }
