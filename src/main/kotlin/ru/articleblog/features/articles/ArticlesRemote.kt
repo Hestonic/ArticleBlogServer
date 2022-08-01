a8d29a26464993a8d94a85e8aef02c0e42574e0d
danii <daniilgeroldovich@gmail.com>
2022-08-01 11:37:54 +0300

    Add response data classes

--- src/main/kotlin/ru/articleblog/features/articles/ArticlesRemote.kt
+++ src/main/kotlin/ru/articleblog/features/articles/ArticlesRemote.kt
@@ -8,3 +8,30 @@ data class ArticleReceiveRemote(
     val text: String,
     val categories: List<Int>,
 )
+
+@Serializable
+data class ArticlesResponseRemote(
+    val articlesList: List<Article>,
+)
+
+@Serializable
+data class Article(
+    val id: Int,
+    val tittle: String,
+    val text: String,
+    val categories: List<Category>,
+    val articleInfo: ArticleInfo,
+)
+
+@Serializable
+data class Category(
+    val id: Int,
+    val category: String,
+)
+
+@Serializable
+data class ArticleInfo(
+    val id: Int,
+    val likes: Int,
+    val views: Int,
+)
